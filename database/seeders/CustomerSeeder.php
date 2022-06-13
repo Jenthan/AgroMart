5ffd848874b88b8abc16f55915e9e55523d98380
Jenthan <jenthanthurai197@gmail.com>
2022-06-13 17:44:03 +0530

    seeding

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class CustomerSeeder extends Seeder
 {
@@ -13,6 +17,6 @@ class CustomerSeeder extends Seeder
      */
     public function run()
     {
-        //
+       //
     }
 }
