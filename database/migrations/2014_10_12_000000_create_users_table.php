8546f9ccf66e278970eeaf7c58472bd00e7fa945
Jenthan <jenthanthurai197@gmail.com>
2022-06-14 12:08:57 +0530

    admin seederupdate

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('role')->default(0);
+            $table->enum('role',['farmer','vender','customer','admin']); 
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
