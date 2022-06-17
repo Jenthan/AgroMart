3224bed47f80c1a8d6e1ed4ea21db9080fe9f316
dulmi <dulmileelarathna1@gmail.com>
2022-06-17 15:14:30 +0530

    reg samm update

--- resources/views/register/userselect.blade.php
+++ resources/views/register/userselect.blade.php
@@ -3,9 +3,9 @@
 <div id="content1">
     <div class="homeloginform high">
     <h1>Select the suitable User Register form...</h1>
-    <button type="button" class="btn btn-primary btn-lg btn-block">Registration as a Farmer</button>
-    <button type="button" class="btn btn-success btn-lg btn-block">Registration as a Vendor</button>
-    <button type="button" class="btn btn-secondary btn-lg btn-block">Registration as a Customer</button>
+    <a href="#"><button type="button" class="btn btn-primary btn-lg btn-block">Registration as a Farmer</button></a>
+    <a href="#"><button type="button" class="btn btn-success btn-lg btn-block">Registration as a Vendor</button></a>
+    <a href="#"><button type="button" class="btn btn-secondary btn-lg btn-block">Registration as a Customer</button></a>
     
 </div>
 </div>
