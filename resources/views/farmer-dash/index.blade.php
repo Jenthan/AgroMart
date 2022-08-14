9c33e316c8fa18b3a7646e506e406cb2bceb7688
[Ajanthanasia] <ajanthanasia@gmail.com>
2022-08-14 13:16:27 +0530

    work in base

--- resources/views/farmer-dash/index.blade.php
+++ resources/views/farmer-dash/index.blade.php
@@ -27,21 +27,21 @@
 				<li>
 					<i class='bx bxs-group' ></i>
 					<span class="text">
-						<h3>2834</h3>
+						<h3>0</h3>
 						<p>Customers</p>
 					</span>
 				</li>
 				<li>
 					<i class='bx bxs-group' ></i>
 					<span class="text">
-						<h3>2834</h3>
+						<h3>0</h3>
 						<p>Vendors</p>
 					</span>
 				</li>
 				<li>
 					<i class='bx bxs-dollar-circle' ></i>
 					<span class="text">
-						<h3>$2543</h3>
+						<h3>Rs. 0.00/=</h3>
 						<p>Total Sales</p>
 					</span>
 				</li>
